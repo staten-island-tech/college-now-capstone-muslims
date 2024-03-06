8efb1d37c69a67597cfbfc9aa4e64e2ad639630c
omerq1 <omerq2307@gmail.com>
2024-03-05 23:07:53 -0500

    shopController

--- Routes/index.js
+++ Routes/index.js
@@ -1,24 +1,6 @@
 const express = require("express");
 const router = new express.Router();
+const shopController = require("../Controllers/shopController");
+router.get("/", shopController.homePage);
 
-router.get("/", (req, res) => {
-  const teacher = { name: "Whalen", subject: "Computer Science", tenure: true };
-  try {
-    // return res.send("We're Live");
-    //res.json(teacher);
-    //can only send back 1 response
-    //res.json(req.query);
-    //can access the request here as well
-  } catch (err) {
-    console.log(err);
-  }
-});
-/*router.get("/teacher/:name", (req, res) => {
-  //try the url /teacher/whalen
-  try {
-    res.json(req.params.name);
-  } catch (err) {
-    console.log(err);
-  }
-}); */
 module.exports = router;
