b893d4b5c6f71d743b666a25e3808cc7eb649171
omerq1 <omerq2307@gmail.com>
2024-03-05 22:43:14 -0500

    Controllers

---
+++ Controller/shopController.js
@@ -0,0 +1,8 @@
+exports.homePage = (req, res) => {
+    const stores = ["Dunkin", "Tim Hortons", "Starbucks"];
+    try {
+      res.json(stores);
+    } catch (error) {
+      console.log(error);
+    }
+  };
