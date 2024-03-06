23708a6563a4431bf4765fca67df7eedb7404df9
omerq1 <omerq2307@gmail.com>
2024-03-05 22:32:43 -0500

    json

--- Routes/index.js
+++ Routes/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
-const router = new express.Router(); //instantiate express router
+const router = new express.Router();
 
 router.get("/", (req, res) => {
+  const teacher = { name: "Whalen", subject: "Computer Science", tenure: true };
   try {
-    return res.send("We're Live");
+    // return res.send("We're Live");
+    //res.json(teacher);
+    //can only send back 1 response
+    //res.json(req.query);
+    //can access the request here as well
+  } catch (err) {
+    console.log(err);
+  }
+});
+router.get("/teacher/:name", (req, res) => {
+  //try the url /teacher/whalen
+  try {
+    res.json(req.params.name);
   } catch (err) {
     console.log(err);
   }
