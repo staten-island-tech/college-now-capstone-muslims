231ecdb4d5cf5610c1cc7dc77c57ac745a5d8394
Ucheong <ucheongsou@gmail.com>
2024-03-06 23:49:49 -0500

    change name

--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = new express.Router();
-const profileController = require("../Controllers/profileController");
+const profileController = require("../Controller/profileController");
 const postController = require("../Controller/postController");
 
 router.post("/add", profileController.createProfile);
