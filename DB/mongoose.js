8dcbdaabfe3d9340d4d83808209928fb5e769899
Ucheong <ucheongsou@gmail.com>
2024-03-07 15:12:59 -0500

    link

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "variables.env" });
 mongoose
-  .connect(`${process.env.DATABASE}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb://atlas-sql-65cd8fe8e2f8bc1f9bcc2b2d-frfqx.a.query.mongodb.net/Collegenow?ssl=true&authSource=admin`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("connected to DB"));
 
 mongoose.connection.on("error", (err) => {
