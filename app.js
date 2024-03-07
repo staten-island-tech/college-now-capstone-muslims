39b86c7ecdf868509b2e6e3eeeedd0e6810da223
Ucheong <ucheongsou@gmail.com>
2024-03-06 23:37:36 -0500

    ASDMOKADNMJIASnUIDNASDNUIANSD

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require("express"); //this file requires express
-const port = process.env.PORT || 3000; //use external server port OR local 3000
-const app = express(); //instantiate express
+const express = require("express");
+const port = process.env.PORT || 3000;
+const app = express();
+require("./DB/mongoose");
 const routes = require("./Routes/index");
+app.use(express.json());
+app.use(express.urlencoded());
 app.use("/", routes);
 app.listen(port, () => {
-  console.log("asdadasdad");
+  console.log(`Server is up on port ${port}`);
 });
