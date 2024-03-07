6f8342d34f5bbec236e7bf9453f53ac48a39709e
omerq1 <omerq2307@gmail.com>
2024-03-06 21:12:53 -0500

    Completed Routes

--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = new express.Router();
 const shopController = require("../Controllers/shopController");
-router.get("/", shopController.homePage);
-
+router.get("/", shopController.homePage); //middlware runs, then when complete, calls next part of requst
+router.post("/add", shopController.createShop);
+router.get("/shops", shopController.getShops);
+router.patch("/shops/:id", shopController.updateShop);
+router.delete("/shops/:id", shopController.deleteShop);
 module.exports = router;
