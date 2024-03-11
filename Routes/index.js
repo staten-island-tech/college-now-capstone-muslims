57904481cfdcce07343e7f4fe2875da0c8bc38e2
omerq1 <omerq2307@gmail.com>
2024-03-11 17:55:23 -0400

    push

--- Routes/index.js
+++ Routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const profileController = require("../Controller/profileController");
 const postController = require("../Controller/postController");
+const authController = require("../Controllers/authController");
 
 router.post("/add", profileController.createProfile);
 router.get("/profiles", profileController.getProfile);
@@ -12,4 +13,8 @@ router.post("/add", postController.createPost);
 router.get("/posts", postController.getPost);
 router.patch("/posts/:id", postController.updatePost);
 router.delete("/posts/:id", postController.deletePost);
+
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.get("/protected", authController.authCheck, authController.protected);
 module.exports = router;
