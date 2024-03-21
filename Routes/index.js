58b1677c88441eac8f7059fa8efd69377dcd9167
Ucheong <ucheongsou@gmail.com>
2024-03-21 15:47:09 -0400

    .

--- Routes/index.js
+++ Routes/index.js
@@ -7,20 +7,24 @@ const authController = require("../Controller/authController");
 router.get("/", profileController.homePage);
 router.get("/", postController.homePage);
 
-router.post("/add", profileController.createProfile);
+router.post("/createProfile", profileController.createProfile);
 router.get("/profiles", profileController.getProfile);
-router.patch("/profiles/:id", profileController.updateProfile);
-router.delete("/profiles/:id", profileController.deleteProfile);
+router.patch("/updateProfiles/:id", profileController.updateProfile);
+router.delete("/deleteProfiles/:id", profileController.deleteProfile);
 
-router.post("/add", postController.createPost);
+router.post("/createPost", postController.createPost);
 router.get("/posts", postController.getPost);
-router.patch("/posts/:id", postController.updatePost);
-router.delete("/posts/:id", postController.deletePost);
+router.patch("/updatePosts/:id", postController.updatePost);
+router.delete("/deletePosts/:id", postController.deletePost);
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/protected", authController.authCheck, authController.protected);
 
-router.post("/uploadTest", profileController.upload, profileController.homePage);
+router.post(
+  "/uploadTest",
+  profileController.upload,
+  profileController.homePage
+);
 router.post("/uploadTest", postController.upload, postController.homePage);
 module.exports = router;
