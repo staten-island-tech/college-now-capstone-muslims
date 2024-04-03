287f3398cd5510590ce75953db4108136b7c6217
Ucheong <ucheongsou@gmail.com>
2024-04-02 21:39:18 -0400

    .

--- collegenow/src/App.vue
+++ collegenow/src/App.vue
@@ -1,85 +1,60 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+    <div>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <div class="Login">
+          <RouterLink class="router" v-if="!loggedin" to="/login"
+            >Login</RouterLink
+          >
+          <button v-if="loggedin" @click="logOut">Log Out</button>
+        </div>
+        <div class="home">
+          <RouterLink> <img id="logo" src="" alt="" /> </RouterLink>
+        </div>
       </nav>
     </div>
   </header>
-
-  <RouterView />
+  <RouterView @loggedin="login" />
 </template>
 
-<style scoped>
+<script>
+const { Router } = require("express");
+</script>
+
+<style>
 header {
   line-height: 1.5;
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
   width: 100%;
-  font-size: 12px;
+  font-size: 20px;
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  color: black;
 }
 
 nav a {
   display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  padding: 1rem;
+  border-left: 2.5px solid var(--color-border);
 }
 
 nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+nav:hover .router {
+  color: #575757;
+}
+nav:hover .router:hover {
+  color: white;
+  box-shadow: 0 2px 0 0 currentColor;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.login {
 }
 </style>
---
+++ collegenow/src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <body>
+      <img id="backgroundImg" src="" alt="" />
+    </body>
+    <form>
+      <div class="loginForm">
+        <h2 class="text">Email</h2>
+        <div class="email">
+          <input type="email" id="email" />
+        </div>
+        <h2 class="text">Password</h2>
+        <div class="password">
+          <input type="password" id="password" />
+        </div>
+        <input type="submit" value="Log In" id="login" @click="login" />
+      </div>
+    </form>
+    <div class="toSignUp">
+      Don't have an account?<a
+        ><RouterLink to="/signup"> Create Account </RouterLink></a
+      >
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.text {
+  margin-top: 1rem;
+}
+#login {
+  margin-top: 1rem;
+}
+</style>
