0e3c3d5125fff129c2cfeac44e79d6ebfdd291c9
Ucheong <ucheongsou@gmail.com>
2024-04-04 16:38:38 -0400

    .

--- collegenow/src/views/PostsView.vue
+++ collegenow/src/views/PostsView.vue
@@ -1,4 +1,12 @@
 <template>
+  <SwipeBox ref="myswipe" @onChange="mySwipeChanged" speed="150">
+    <img :src="image.url" />
+  </SwipeBox>
+
+  <button @click="next">next</button><br />
+  <button @click="previous">previous</button><br />
+  <button @click="goto2">goto index #2</button><br />
+
   <div class="Posts">
     <Post
       v-for="post in uploadedPhotos"
@@ -19,14 +27,35 @@
 </template>
 
 <script>
+import SwipeBox from "@shopid/vue3-swipe-box";
 import Post from "../components/Post.vue";
+
 export default {
+  components: {
+    SwipeBox,
+  },
   data() {
     return {
       uploadedPhotos: [
         // Add more photos as they are uploaded from back end
       ],
     };
+  },
+
+  methods: {
+    mySwipeChanged: function (index) {
+      console.log("index" + index);
+    },
+
+    next: function (x) {
+      this.$refs.myswipe.swipetoNext();
+    },
+    previous: function (x) {
+      this.$refs.myswipe.swipetoPrevious();
+    },
+    goto2: function () {
+      this.$refs.myswipe.goTo(2);
+    },
   },
 };
 </script>
