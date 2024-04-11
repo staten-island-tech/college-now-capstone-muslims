ece0468ee495ea004b44bd5dec6eefc152522173
omerq1 <149286864+omerq1@users.noreply.github.com>
2024-04-11 16:44:39 -0400

    xwf

---
+++ collegenow/src/views/ProfileView.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+      
+  }
+async function createProfile() {
+try {
+  const res = await fetch("http://localhost:3000/createProfile", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+
+    }),
+  });
+  const user = await res.json();
+  console.log(user);
+} catch (error) {
+  console.log(error);
+}
+}
+
+async function profiles(username, password) {
+try {
+  const res = await fetch("http://localhost:3000/profiles", {
+    method: "DEL",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      
+    }),
+  });
+  const user = await res.json();
+  console.log(user);
+} catch (error) {
+  console.log(error);
+}
+}
+
+async function updateProfiles() {
+try {
+  const res = await fetch("http://localhost:3000/updateProfiles/:id", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+
+    }),
+  });
+  const user = await res.json();
+  console.log(user);
+} catch (error) {
+  console.log(error);
+}
+}
+
+async function deleteProfiles() {
+try {
+  const res = await fetch("http://localhost:3000/deleteProfiles/:id", {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+
+    }),
+  });
+  const user = await res.json();
+  console.log(user);
+} catch (error) {
+  console.log(error);
+}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- collegenow/src/views/SignUpView.vue
+++ collegenow/src/views/SignUpView.vue
@@ -78,6 +78,24 @@ export default {
     };
   },
 };
+
+async function register() {
+  try {
+    const res = await fetch("http://localhost:3000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        
+      }),
+    });
+    const user = await res.json();
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  }
+}
 </script>
 
 <style></style>
