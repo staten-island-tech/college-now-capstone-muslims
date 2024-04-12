1dc6320db3aeb59ec277f8f861c0b7bd8439f2f0
omerq1 <omerq2307@gmail.com>
2024-04-11 22:44:33 -0400

    x

--- collegenow/src/views/PostsView.vue
+++ collegenow/src/views/PostsView.vue
@@ -35,5 +35,83 @@ export default {
 
   methods: {},
 };
+
+async function  createPost(Name, ownerName, number, Description) {
+  try {
+    const res = await fetch("http://localhost:3000/createPost", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        Name: Name,
+        ownerName: ownerName,
+        number: number,
+        Description: Description,
+      }),
+    });
+    const user = await res.json();
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function posts(username, password) {
+  try {
+    const res = await fetch("http://localhost:3000/posts", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username.toLowerCase(),
+        password: password,
+      }),
+    });
+    const user = await res.json();
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updatePosts(username, password) {
+  try {
+    const res = await fetch("http://localhost:3000/updatePosts/:id", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username.toLowerCase(),
+        password: password,
+      }),
+    });
+    const user = await res.json();
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function deletePosts(username, password) {
+  try {
+    const res = await fetch("http://localhost:3000/deletePosts/:id", {
+      method: "DEL",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username.toLowerCase(),
+        password: password,
+      }),
+    });
+    const user = await res.json();
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  }
+}
 </script>
 <style></style>
