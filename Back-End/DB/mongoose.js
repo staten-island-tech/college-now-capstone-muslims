63f260ae9412dd5699f9ed152531ea3ebd2ec031
Ucheong <ucheongsou@gmail.com>
2024-05-20 02:50:22 -0400

    .

--- Back-End/DB/mongoose.js
+++ Back-End/DB/mongoose.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "variables.env" });
-mongoose.connect(
-  `${process.env.DATABASE}`
-  // `mongodb+srv://ucheong:b2ROe5Tji7raT2Ff@collegenow.oopvzbr.mongodb.net/data`
-);
+mongoose.connect(`${process.env.DATABASE}`);
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB");
 });
